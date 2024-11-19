55daecb1ac383f4b47e4595c3b7da87b432afe66
ahmad <aza29@mail.aub.edu>
2024-11-20 00:53:59 +0200

    Refactor Task model: Use Uuid for id and update fields
    Replaced integer id with Uuid for better uniqueness and removed manual id management. Added a reminderDateTime field and reordered variable declarations for clarity.
    Also made subTasks public since the list is declared as a val and it's safe to access it directy.

--- composeApp/src/commonMain/kotlin/aza161/test/todolist/models/Task.kt
+++ composeApp/src/commonMain/kotlin/aza161/test/todolist/models/Task.kt
@@ -6,6 +6,8 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.serializers.InstantIso8601Serializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @Serializable
 data class Task(
@@ -14,31 +16,19 @@ data class Task(
     var dueDate: LocalDateTime? = null,
     var priority: Int = 0
 ) : Comparable<Task> {
-    public val id: Int = Task.nextID()
+
+    public var complete: Boolean = false
+    public var remindeDateTime: LocalDateTime? = null
+
+    @OptIn(ExperimentalUuidApi::class)
+    public val id: Uuid = Uuid.random()
 
     @Transient
-    private val subTasks: MutableList<Task> = mutableListOf()
+    public val subTasks: MutableList<Task> = mutableListOf()
 
     @Serializable(with = InstantIso8601Serializer::class)
     private val timestamp: Instant = Clock.System.now()
-    public var complete: Boolean = false
-
-    companion object {
-        private var idCounter = 0
-
-        fun nextID(): Int {
-            return idCounter++
-        }
-    }
-
-    fun addSubtask(subtask: Task): Unit {
-        this.subTasks.add(subtask)
-    }
-
-    fun getSubtasks(): List<Task> {
-        return this.subTasks.toList()
-    }
-
+    
     override fun compareTo(other: Task): Int {
         return when {
             (this.priority == other.priority) -> this.timestamp.compareTo(other.timestamp)
