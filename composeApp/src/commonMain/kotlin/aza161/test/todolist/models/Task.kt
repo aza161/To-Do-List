40879452f5ae530cdee94e8695bee50be021e8de
ahmad <aza29@mail.aub.edu>
2024-11-24 03:19:10 +0200

    Add detailed KDoc comments to Task class properties and methods
    This commit enhances the documentation for the Task class by adding detailed KDoc comments. It provides descriptions for the properties and methods of the class, aiding in better understanding and maintenance.

--- composeApp/src/commonMain/kotlin/aza161/test/todolist/models/Task.kt
+++ composeApp/src/commonMain/kotlin/aza161/test/todolist/models/Task.kt
@@ -1,3 +1,24 @@
+/**
+ * @author aza161
+ *
+ * A class that represents a Task in a ToDoList app.
+ *
+ * A task can have a name, description, due date, priority, and optional reminder.
+ * Tasks are comparable based on their priority and timestamp.
+ * @see [Comparable]
+ * @see [compareTo]
+ *
+ * @property name Represents the name of the task.
+ * @property description Represents the description of the task.
+ * @property dueDate Represents the due date of the task.
+ * @property complete Represents if the task is completed or not.
+ * @property reminderDateTime Represents the reminder date and time of the task.
+ * @property id Represents the unique universal id of the task.
+ * @property subTasks Represents the list of subtasks of the task.
+ * @property timestamp Represents the initialization timestamp of the task.
+ * @property priority Represents the priority of the task. It ranges between 0 and 10.
+ */
+
 package aza161.test.todolist.models
 
 import kotlinx.datetime.LocalDateTime
@@ -28,12 +49,28 @@ data class Task(
     @Serializable(with = InstantIso8601Serializer::class)
     public val timestamp: Instant = Clock.System.now()
 
+    /**
+     * @property priority Represents the priority of the task. It ranges between 0 and 10. Default value is 0.
+     */
     public var priority: Int = 0
+        /**
+         * Sets the priority of the task.
+         * The priority must be within the range of 0 to 10.
+         * @param priority The priority to set, which should be between 0 (lowest) and 10 (highest).
+         * @throws IllegalArgumentException if the priority is not within the valid range.
+         */
         set(priority: Int) {
             require(priority in 0..10) { "Priority should be between 0 and 10, priority = $priority" }
             field = priority
         }
 
+    /**
+     * Compares this Task with the specified other Task for order.
+     * The comparison is primarily based on the priority; if the priority is the same, the timestamp is compared.
+     * @param other the Task to be compared.
+     * @return zero if this Task is equal to the other Task, a negative number if it is less,
+     * and a positive number if it is greater.
+     */
     override fun compareTo(other: Task): Int {
         return when {
             (this.priority == other.priority) -> this.timestamp.compareTo(other.timestamp)
